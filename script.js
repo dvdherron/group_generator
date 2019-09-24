a81f8f69010443a71b5a0c67da858edc146270ba
dvdherron <dvdherron@gmail.com>
2019-09-24 11:08:48 +0200

    Add script, make size of items dynamic
    Why:
    
    * Implements functionality for changing names on circles
    
    This change addresses the need by:
    
    * allows circles and text to expand as viewport width adjusts

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-script goes here
+
+
+(function () {
+
+  var classMates = ['david h', 'sigge', 'eva', 'august', 'Åsa', 'lina', 'david l', 'katja', 'duygu', 'caglar', 'jakob', 'anders', 'arlind', 'bob', 'calle', 'dipak', 'douglas', 'emelie e', 'emelie s', 'emil', 'emilio', 'essi', 'fanny', 'gina', 'joakim', 'johan', 'mudassar', 'nayab', 'peter', 'quena', 'tim', 'tom', 'tony', 'ture', 'wei'];
+
+  
+  var circle1 = document.getElementById('circle1');
+  circle1.addEventListener('click', pickMate);
+  var circle2 = document.getElementById('circle2');
+  circle2.addEventListener('click', pickMate);
+  var circle3 = document.getElementById('circle3');
+  circle3.addEventListener('click', pickMate);
+  // var circleTitle = document.querySelector('.circle_title');
+  var startOver = document.getElementById('resetBTN');
+  // startOver.addEventListener('click', resetNames);
+
+  function pickMate(event) {
+    var randomMate = classMates[Math.floor(Math.random()*classMates.length)];
+
+    event.target.firstElementChild.textContent = randomMate;
+    }
+
+  // function resetNames() {
+  //   var circles = document.getElementsByClassName('.circle_title');
+  //   circles.textContent = '- - -';
+  // }
+
+  }());
